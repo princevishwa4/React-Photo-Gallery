e84471c7419425a3556569ac10f43cfe0552587d
princevishwa4 <princestudy4@gmail.com>
2021-05-13 15:05:32 +0530

    firebase configuration added

---
+++ src/Firebase/Config.js
@@ -0,0 +1,18 @@
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/storage';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBYB7qKQvgPbpIfbeuA77NIPeGV92kPb1A",
+    authDomain: "react-photo-gallery-4531c.firebaseapp.com",
+    projectId: "react-photo-gallery-4531c",
+    storageBucket: "react-photo-gallery-4531c.appspot.com",
+    messagingSenderId: "641084821480",
+    appId: "1:641084821480:web:f6b20fed57468ed3fb5e5d"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage();
+const projectStorage = firebase.firestore();
